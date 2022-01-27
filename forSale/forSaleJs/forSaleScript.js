a67a31d06c7e70b643a41930f392ba3aa9c3c680
Gayane25 <gayabennu@gmail.com>
2022-01-27 12:12:12 +0400

    feat:changed functionality for popUp page

--- forSale/forSaleJs/forSaleScript.js
+++ forSale/forSaleJs/forSaleScript.js
@@ -1,13 +1,25 @@
 const open = document.getElementById('open');
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 const modal_container = document.getElementById('modal-container');
 const close = document.getElementById('close');
+// const overlay = document.getElementById('overlay');
+// open.onclick = function () {
+//   modal_container.style.display = 'flex';
+//   //   body.style.backgroundColor = 'rgba(0,0,0,0.3)';
+// };
+// close.onclick = function () {
+//   modal_container.style.display = 'none';
+//   body.style.backgroundColor = '#edeef6';
+// };
 
-open.onclick = function () {
+open.addEventListener('click', function () {
   modal_container.style.display = 'flex';
-  body.style.backgroundColor = 'rgba(0,0,0,0.3)';
-};
-close.onclick = function () {
+});
+
+close.addEventListener('click', function () {
   modal_container.style.display = 'none';
-  body.style.backgroundColor = '#edeef6';
-};
+});
+
+// overlay.addEventListener('click', function () {
+//   modal_container.style.display = 'none';
+// });
