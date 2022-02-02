507650df291c41956a6b4559789e0fb2258240ad
Gayane25 <gayabennu@gmail.com>
2022-02-02 20:09:15 +0400

    feat:commented divs in projectPage,added obj for div elements

---
+++ projectsPage/projectsJS/script.js
@@ -0,0 +1,15 @@
+'use strict';
+const projectObj = {
+  title: 'Life Valley Project',
+  p1: 'The privileged location in Lambeth region in the west of London city',
+  p2: 'The project is close to many hotels,hospitals and commercial centers...',
+  btnText: 'View Project',
+  img: { url: '//assets/lifeproject.png' },
+};
+// const projects = document.getElementById('lifeProjects');
+// const textSide = document.createElement('div');
+// textSide.className = 'life-project-text';
+// projects.append(textSide);
+// const projectTitle = document.createElement('h4');
+// projectTitle.textContent = 'Life Valley Project';
+// projects.append(projectTitle);
