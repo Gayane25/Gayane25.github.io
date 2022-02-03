65b44f5912e5a4a8dbb66e80cac3c0f506313ac1
Gayane25 <gayabennu@gmail.com>
2022-02-03 11:15:42 +0400

    feat:added picture and text to projectPage.html

--- projectsPage/projectsJS/script.js
+++ projectsPage/projectsJS/script.js
@@ -4,12 +4,44 @@ const projectObj = {
   p1: 'The privileged location in Lambeth region in the west of London city',
   p2: 'The project is close to many hotels,hospitals and commercial centers...',
   btnText: 'View Project',
-  img: { url: '//assets/lifeproject.png' },
 };
-// const projects = document.getElementById('lifeProjects');
-// const textSide = document.createElement('div');
-// textSide.className = 'life-project-text';
-// projects.append(textSide);
-// const projectTitle = document.createElement('h4');
-// projectTitle.textContent = 'Life Valley Project';
-// projects.append(projectTitle);
+const lifeProjects = document.getElementById('lifeProjects');
+lifeProjects.className = 'life-projects';
+const singleProject = document.createElement('div');
+lifeProjects.append(singleProject);
+const leftSide = document.createElement('div');
+leftSide.className = 'left-side';
+
+const rightSide = document.createElement('div');
+rightSide.className = 'right-side';
+const imgres = document.createElement('img');
+imgres.setAttribute('src', '../../assets/lifeproject.png');
+singleProject.append(leftSide, rightSide);
+rightSide.append(imgres);
+
+function getTitle() {
+  const projectTitle = document.createElement('h4');
+  projectTitle.className = 'h4title';
+  projectTitle.textContent = `${projectObj.title}`;
+  leftSide.append(projectTitle);
+}
+getTitle();
+
+function getParagraphs() {
+  const p1 = document.createElement('p');
+  p1.className = 'project-paragraph';
+  p1.textContent = `${projectObj.p1}`;
+  const p2 = document.createElement('p');
+  p2.className = 'project-paragraph';
+  p2.textContent = `${projectObj.p2}`;
+  leftSide.append(p1, p2);
+}
+getParagraphs();
+
+function getBtnBox() {
+  const btn = document.createElement('button');
+  btn.className = 'hoverButton';
+  btn.textContent = `${projectObj.btnText}`;
+  leftSide.append(btn);
+}
+getBtnBox();
