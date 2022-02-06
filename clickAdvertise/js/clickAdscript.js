d5acf750c14a81e0f70af11ba2d3cd24fe43a439
Gayane25 <gayabennu@gmail.com>
2022-02-06 21:34:56 +0400

    feat:changed directory for clickAdscript.js

--- clickAdvertise/js/clickAdscript.js
+++
@@ -1,8 +0,0 @@
-// const butt = document.getElementById('clickButt');
-// butt.addEventListener('click', function () {
-//   butt.type = 'file';
-// });
-const butt = document.getElementById('clickButt');
-butt.addEventListener('click', function () {
-  butt.type = 'file';
-});
